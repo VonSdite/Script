c62cf5f2b7c05ec668276fdd8f1e1c30cbaa5839
Little Von <122691411@qq.com>
2018-02-04 10:13:57 +0800

    Initial commit

---
+++ changeToJPEG/README.md
@@ -0,0 +1,28 @@
+# 将图片转化为JPEG格式
+
+## 使用方法
+
+1. 先将该项目'changeToJPEG'文件夹路径加入到**环境变量**中(这样更方便使用)
+
+2. 然后就可以如下操作:
+
+**changeToJPEG.py**
+*介绍: changeToJPEG.py通过命令行参数一次转化一张或多张图片为JPEG格式*
+
+- 命令行输入
+```
+changeToJPEG.py 图片名1.png [图片名2.png ...]
+```
+
+**change.py**
+*介绍: change.py 通过命令行将当前目录下所有图片转化为JPEG格式，并保存在JPEG文件夹中*
+
+- 命令行输入
+```
+change.py
+```
+
+### BTW
+- change.py 属于批量处理，方便你的操作
+- changeToJPEG.py 需要手动打上图片的名字或者路径，比较麻烦一点
+- 当然，最重要的要加入到**环境变量**中！！这样才方便实用
---
+++ changeToJPEG/change.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# __Author__: Sdite
+# __Email__ : a122691411@gmail.com
+
+import os, sys
+from PIL import Image
+
+def change():    
+    PIC_FORMAT = ['.png', '.gif', '.bmp']   # 要转化为JPEG格式的图片格式
+
+    # 当前目录下创建保存结果JPEG的文件夹
+    JPEG = 'JPEG/'
+    if not os.path.isdir(JPEG):
+        os.mkdir(JPEG)
+
+    # 修改当前路径下所有图片为JPEG
+    imageSet = os.listdir('.')
+    for inFile in imageSet:
+        f, e = os.path.splitext(inFile)
+
+        # 判断是否是图片
+        if e not in PIC_FORMAT:
+            continue        # 不是则continue 
+
+        outFile = JPEG + f + '.jpg'
+
+        try:
+            img = Image.open(inFile)
+        except FileNotFoundError as e:
+            print('无法打开文件{}'.format(inFile))
+            continue
+
+        # png图片有四个通道RGBA, 而JPEG只有三个通道，所以如果是四通道，则只取前三个通道
+        if len(img.split()) == 4:
+            r, g, b, a = img.split()
+            img = Image.merge("RGB", (r, g, b))
+
+        # 进行格式转化
+        try:
+            img.save(outFile)
+        except IOError as e:
+            print("Can't convert {} //".format(inFile), e)
+            os.system('del {}'.format(outFile)) # 删除转化错误的文件
+
+if __name__ == '__main__':
+    change()
+
